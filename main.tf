7046d30443eac9d94e5e9c580a7639e60ad1e0b1
github_julijaand <julijaand@yahoo.com>
2023-10-13 16:04:13 +0300

    custom metrics created

--- main.tf
+++ main.tf
@@ -7,22 +7,9 @@ resource "aws_instance" "JA_client_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+  iam_instance_profile = "role-d4ml-cloud9-deployment" 
 
-  user_data = <<-EOF
-    #!/bin/bash
-      tar -czvf /tmp/varlog.tar.gz /var/log/
-
-      aws s3 cp /tmp/varlog.tar.gz s3://j.andrusenko-bucket/500mb_dir/
-
-      rm /tmp/varlog.tar.gz
-
-      aws s3 ls s3://j.andrusenko-bucket/500mb_dir/ > /tmp/uploaded_files.txt
-
-      aws s3 cp /tmp/uploaded_files.txt s3://j.andrusenko-bucket/500mb_dir/
-
-      INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-      aws ec2 terminate-instances --instance-ids $INSTANCE_ID --region eu-central-1
-      EOF
+  user_data = "C:/Users/julija.andrusenko/Desktop/projects/D4ML_AWS/upload_and_list.sh"
 
   tags = {
   Name = "JA_client_instance"
